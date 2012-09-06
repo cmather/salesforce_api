205bc20ca45880a1db4ac0e2f1765dcab10dffc4
Chris Mather <mather.chris@gmail.com>
2012-09-06 16:10:33 -0700

    Removed old method.

--- lib/salesforce/data_api.rb
+++ lib/salesforce/data_api.rb
@@ -5,17 +5,6 @@ module Salesforce
       "/services/data/v25.0"
     end
 
-    def do_request verb, path, opts={}, &block
-      access_token.send verb, path, opts, &block 
-    rescue OAuth2::Error => e
-      if e.response.status == 401
-        get_token
-        do_request verb, path, opts, &block
-      else
-        raise e
-      end
-    end
-
     def query soql
       request :get, "/query" do |req|
         req.params['q'] = soql
